fbb484fe306d87c32b97431dd403216226c9ab3d
MKRyuto <randiprawira23@gmail.com>
2021-02-24 15:01:00 +0700

    Mengosongkan Array items

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const date = require(__dirname + "/date.js")
 
 const app = express();
 
-const items = ['Playing Arknight', 'Playing Genshin'];
+const items = [];
 const workItems = [];
 
 app.set('view engine', 'ejs');
